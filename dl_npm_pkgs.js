8ef1204ca181b2b5c73352415fbdb21bd7ea5ff1
vincentp3 <95020790+vincentp3@users.noreply.github.com>
2023-04-14 09:21:52 +0200

    Update dl_npm_pkgs.js

--- dl_npm_pkgs.js
+++ dl_npm_pkgs.js
@@ -3,44 +3,57 @@ const { execSync } = require('child_process');
 
 const packageLock = JSON.parse(fs.readFileSync('npm-shrinkwrap.json', 'utf8'));
 
+function getLatestVersion(name) {
+  try {
+    const output = execSync(`npm view ${name} version`, { encoding: 'utf8' });
+    return output.trim();
+  } catch (error) {
+    console.error(`Erreur lors de la récupération de la dernière version de ${name}:`, error.message);
+    return 'latest';
+  }
+}
+
 function downloadPackage(name, version) {
   const packageName = `${name}@${version}`;
   console.log(`Downloading ${packageName}`);
   execSync(`npm pack ${packageName}`, { stdio: 'inherit' });
 }
-function processDep(deps){
-  for (const key in deps){
+
+function processDep(deps) {
+  for (const key in deps) {
     let value = deps[key];
-    var version = value;
-    if (version.includes("^")){
-      version = "latest";
+    let version = value;
+    if (version.includes('^')) {
+      version = getLatestVersion(key);
     }
     downloadPackage(key, version);
   }
 }
+
 function processDependencies(deps) {
   for (const [name, info] of Object.entries(deps)) {
-    console.log("Traitement de ", name, info);
+    console.log('Traitement de ', name, info);
     if (name === '') {
       continue; // Ignore root project
     }
-    var short_name = name;
-    if (name.includes("/")){
-    var parts = name.split("/");
-    var short_name_parts = parts.slice(1);
-    short_name = short_name_parts.join("/");
-    if (short_name.includes("node_modules/")){
-      parts = short_name.split("node_modules/");
-      short_name = parts[parts.length - 1];
-      console.log("short_name = ", short_name);
+    let short_name = name;
+    if (name.includes('/')) {
+      const parts = name.split('/');
+      const short_name_parts = parts.slice(1);
+      short_name = short_name_parts.join('/');
+      if (short_name.includes('node_modules/')) {
+        parts = short_name.split('node_modules/');
+        short_name = parts[parts.length - 1];
+        console.log('short_name = ', short_name);
+      }
     }
-    }
-    
+
     downloadPackage(short_name, info.version);
     if (info.dependencies) {
       processDep(info.dependencies);
     }
   }
 }
+
 console.log(packageLock);
 processDependencies(packageLock.packages);
